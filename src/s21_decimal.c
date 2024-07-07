d300aead0644d4b0a4ad05d00a09b415d26f4081
asadaker <asadaker@student.21-school.ru>
2024-07-07 18:29:44 +0700

    Hello

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -42,3 +42,36 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   }
   return return_value;
 }
+
+int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+  // sub - subtraction
+  int resurn_value = OK, sub;
+  for (int position = 0; position < 95; position++) {
+    sub = get_bit(value_1, position) - get_bit(value_2, position);
+    if (sub == 1) {
+      set_bit(&result, position);
+    } else if (sub == -1) {
+      
+    }
+  }
+}
+
+/*
+  Правила вычитания:
+  1) Разница битов = 0
+    1.1) Результат = 0
+
+  2) Разница битов = 1
+    2.1) Результат = 1
+
+  3) Разница битов = -1
+    3.1) Ищем более старший бит, который равен 1
+    3.2) Занимаем у него единицу, он становится 0
+    3.3) Все относительно него младшие биты становятся 1,
+         а тот бит, для которого занимали - становится 10, то есть 2
+    3.4) Производим вычитание, то есть бит, для которого мы занимали единицу,
+         становится 1
+
+  Поиск старшего единичного бита относительно рассматриваемого:
+
+*/
