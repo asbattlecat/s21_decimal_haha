dac015a43829c8c686d9c75f041e54d12b8443e4
jeffielo <zaikinevgeny101@gmail.com>
2024-07-19 11:55:56 +0200

    jeffielo: added files

---
+++ src/s21_decimal.c
@@ -0,0 +1,6 @@
+#include "s21_decimal.h"
+
+int main() {
+    s21_decimal value;
+    
+}
---
+++ src/s21_decimal.h
@@ -0,0 +1,10 @@
+#ifndef S21_DECIMAL
+#define S21_DECIMAL
+
+#include <stdio.h>
+
+typedef struct {
+    unsigned int bits[4];
+} s21_decimal;
+
+#endif
