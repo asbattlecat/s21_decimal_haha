7d6f23dca204100aa47a5984cc6b99b0b94fd592
loralech <loralech@DESKTOP-C6DOPDR>
2024-07-21 03:19:05 +0700

    сонный коммит

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,21 +1,19 @@
 #define MINUS 0x80000000  // sing of minus 10000000 00000000 00000000 00000000
 #define SCALE 0x00ff0000  // thats scaaale 00000000 11111111 00000000 00000000
-#define FULLBITES \
-  0xffffffff  // 4 bits are full 11111111 11111111 11111111 11111111
+#define FULLBITES 0xffffffff  // 4 bits are full 11111111 11111111 11111111 11111111
 
 //наш классический децимал
 typedef struct {
   int bits[4];
 } s21_decimal;
 
-//децимал для вычислений, жирненький слегка
+//децимал для вычисления суммы и разности
 typedef struct {
   long int bits[7];  // будет использоваться бит x2
   short int scale;  // для scale достаточно маленького инта
 } work_decimal;
 
-//мне подсказал Бимба, когда курили у горячего Цеха что можно сделать бигдецимал
-//из двух обычных - и я решил что это просто отличная идея.
+//децимал для вычисления умножения и деления
 typedef struct {
   s21_decimal decimal[2];
 } big_decimal;
